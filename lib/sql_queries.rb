64c4d904eb9b7ef4e8562492a4304ae41e456450
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-13 01:10:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT NAME, AGE FROM BEARS ORDER BY AGE LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
